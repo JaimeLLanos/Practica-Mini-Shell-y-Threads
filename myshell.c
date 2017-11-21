714da1c2c57b807d481752fa3cc3828e57f340e5
JaimeLlanos <jaime.jlm@hotmail.com>
2017-11-21 19:50:26 +0100

    medio Puntico - con chorizaco de errores
    :D

--- myshell.c
+++ myshell.c
@@ -7,16 +7,60 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void uncomando(tcommand *mandato){
+void *redireccionDeEntrada(tline *mandato){
+	int fd;	
+	if((*mandato).redirect_input != NULL){
+		fd = open((*mandato).redirect_input,O_RDONLY);
+		if(fd == -1){
+			 printf(stderr,"%s: Error. No existe el fichero.\n",(*mandato).redirect_input);
+			exit(1);
+		}else{
+			dup2(fd,0); //El 0 indica que es de lectura
+		}
+	}
+}
+
+void *redireccionDeSalida(tline *mandato){
+	int fd;
+	if((*mandato).redirect_output != NULL){
+		fd = open((*mandato).redirect_output,O_WONLY|O_CREAT);
+		if(fd == -1){
+			printf(stderr,"%s: Error. Fallo al crear el fichero para escritura.",(*mandato).redirect_output);
+			exit(1);
+		}else{
+			dup2(fd,1); //El 1 indica que es de escritura
+		}
+	}
+	
+}
+
+void *redireccionDeError(tline *mandato){
+	int fdesc;
+	if((*mandato).redirect_error != NULL){
+		fd = open((*mandato).redirect_error);		
+		if(fd == -1){
+			printf(stderr,"%s: Error. Fallo al crear el fichero para su escritura",(*mandato).redirect_error);
+			exit(1);
+		}else{
+			dup2(fd,2); //El 2 indica que es de error
+		}
+	}
+}
+
+void uncomando(tline *mandatos){
 	pid_t pid;
+	tcommand *mandato = mandatos.commands;
 	pid=fork();
 	if (pid<0) {
 		printf("fallo en el fork.\n");
 	} else if (pid==0) {
 		signal(SIGQUIT, SIG_DFL);
 		signal(SIGINT, SIG_DFL);
+		redireccionDeEntrada(mandatos);
+		redireccionDeSalida(mandatos);
+		redireccionDeError(mandatos);
 		if((*mandato).filename == NULL){
-			printf("El mandato especificado no existe\n");
+			printf(stderr,"%s: No se encuentra el mandato\n",(*mandato).filename;
 			exit(1);
 		}else{
 			execvp((*mandato).filename,(*mandato).argv);
@@ -36,15 +80,15 @@ void varioscomandos(){
 int main(int argc, char* argv[]){ //inicio main
 	signal(SIGQUIT, SIG_IGN);
 	signal(SIGINT, SIG_IGN);
-	tline *mandatos; //variable de hacer tokenize
+	tline *mandatos; //variable para hacerle el tokenize
 	char buffer[1024];
 	if (argc==1) {
-		while (1) { //inicio de while
-			printf("msh> ");
-			fgets(buffer, 1024, stdin); //recogemos en buffer la variable de entrada de teclado
+		while (fgets(buffer, 1024, stdin)) { //inicio de while
+			//recogemos en buffer la variable de entrada de teclado
+			printf("msh> ");			
 			mandatos=tokenize(buffer);
 			if ((*mandatos).ncommands==1) {
-				uncomando((*mandatos).commands);
+				uncomando(mandatos);
 			} else if ((*mandatos).ncommands>1) {
 				varioscomandos();
 			}
