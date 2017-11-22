326ac8795e4af3b2168260af7e66672ac36898f0
JaimeLlanos <jaime.jlm@hotmail.com>
2017-11-22 12:08:38 +0100

    2 puntitos y medio; sin errores muahahaha

--- myshell.c
+++ myshell.c
@@ -6,13 +6,15 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+
 
 void *redireccionDeEntrada(tline *mandato){
 	int fd;	
 	if((*mandato).redirect_input != NULL){
 		fd = open((*mandato).redirect_input,O_RDONLY);
 		if(fd == -1){
-			 printf(stderr,"%s: Error. No existe el fichero.\n",(*mandato).redirect_input);
+			 fprintf(stderr,"%s: Error. No existe el fichero.\n",(*mandato).redirect_input);
 			exit(1);
 		}else{
 			dup2(fd,0); //El 0 indica que es de lectura
@@ -23,9 +25,9 @@ void *redireccionDeEntrada(tline *mandato){
 void *redireccionDeSalida(tline *mandato){
 	int fd;
 	if((*mandato).redirect_output != NULL){
-		fd = open((*mandato).redirect_output,O_WONLY|O_CREAT);
+		fd = open((*mandato).redirect_output,O_WRONLY | O_CREAT | O_TRUNC);
 		if(fd == -1){
-			printf(stderr,"%s: Error. Fallo al crear el fichero para escritura.",(*mandato).redirect_output);
+			fprintf(stderr,"%s: Error. Fallo al crear el fichero para escritura.",(*mandato).redirect_output);
 			exit(1);
 		}else{
 			dup2(fd,1); //El 1 indica que es de escritura
@@ -35,11 +37,11 @@ void *redireccionDeSalida(tline *mandato){
 }
 
 void *redireccionDeError(tline *mandato){
-	int fdesc;
+	int fd;
 	if((*mandato).redirect_error != NULL){
-		fd = open((*mandato).redirect_error);		
+		fd = open((*mandato).redirect_error, O_WRONLY | O_CREAT | O_TRUNC);		
 		if(fd == -1){
-			printf(stderr,"%s: Error. Fallo al crear el fichero para su escritura",(*mandato).redirect_error);
+			fprintf(stderr,"%s: Error. Fallo al crear el fichero para su escritura",(*mandato).redirect_error);
 			exit(1);
 		}else{
 			dup2(fd,2); //El 2 indica que es de error
@@ -49,22 +51,22 @@ void *redireccionDeError(tline *mandato){
 
 void uncomando(tline *mandatos){
 	pid_t pid;
-	tcommand *mandato = mandatos.commands;
+	tcommand *mandato = (*mandatos).commands;
 	pid=fork();
-	if (pid<0) {
-		printf("fallo en el fork.\n");
-	} else if (pid==0) {
+	if (pid < 0) {
+		fprintf(stderr,"fallo en el fork.\n");
+	} else if (pid == 0) {
 		signal(SIGQUIT, SIG_DFL);
 		signal(SIGINT, SIG_DFL);
 		redireccionDeEntrada(mandatos);
 		redireccionDeSalida(mandatos);
 		redireccionDeError(mandatos);
 		if((*mandato).filename == NULL){
-			printf(stderr,"%s: No se encuentra el mandato\n",(*mandato).filename;
+			fprintf(stderr,"%s: No se encuentra el mandato\n",(*mandato).filename);
 			exit(1);
 		}else{
 			execvp((*mandato).filename,(*mandato).argv);
-			printf("error execvp\n");
+			fprintf(stderr,"error execvp\n");
 			exit(1);		
 		}
 	} else { 
@@ -82,17 +84,18 @@ int main(int argc, char* argv[]){ //inicio main
 	signal(SIGINT, SIG_IGN);
 	tline *mandatos; //variable para hacerle el tokenize
 	char buffer[1024];
-	if (argc==1) {
+	printf("msh> ");
+	if (argc == 1) {
 		while (fgets(buffer, 1024, stdin)) { //inicio de while
 			//recogemos en buffer la variable de entrada de teclado
 			printf("msh> ");			
-			mandatos=tokenize(buffer);
-			if ((*mandatos).ncommands==1) {
+			mandatos = tokenize(buffer);
+			if ((*mandatos).ncommands == 1) {
 				uncomando(mandatos);
-			} else if ((*mandatos).ncommands>1) {
+			} else if ((*mandatos).ncommands > 1) {
 				varioscomandos();
 			}
 		} //fin del while
 	} else //fin del if
-		printf("Error, funcion sin argumentos.\n");
+		fprintf(stderr,"Error, funcion sin argumentos.\n");
 } //fin del main
