c57b495483ae9f72bf58c050c6a150a2ded72a6c
JaimeLlanos <jaime.jlm@hotmail.com>
2018-01-05 13:48:19 +0100

    el variosComandos funciona a medias

--- myshell.c
+++ myshell.c
@@ -8,6 +8,7 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
+//Estructura de tipo de dato tPipe, para facilitar el trabajo con el array de pipes en el método "variosComandos"
 typedef struct {
 	int p[2];
 } tPipe;
@@ -16,12 +17,12 @@ typedef struct {
 void *redireccionDeEntrada(tline *mandato){
 	int fd;	
 	if((*mandato).redirect_input != NULL){
-		fd = open((*mandato).redirect_input,O_RDONLY);
+		fd = open((*mandato).redirect_input,O_RDONLY); //cogemos el descriptor de fichero asociado a nuestro mandato
 		if(fd == -1){
-			 fprintf(stderr,"%s: Error. No existe el fichero.\n",(*mandato).redirect_input);
+			fprintf(stderr,"%s: Error. No existe el fichero.\n",(*mandato).redirect_input);
 			exit(1);
 		}else{
-			dup2(fd,0); //El 0 indica que es de lectura
+			dup2(fd,0); //El 0 indica que es entrada de lectura
 		}
 	}
 }
@@ -34,7 +35,7 @@ void *redireccionDeSalida(tline *mandato){
 			fprintf(stderr,"%s: Error. Fallo al crear el fichero para escritura.",(*mandato).redirect_output);
 			exit(1);
 		}else{
-			dup2(fd,1); //El 1 indica que es de escritura
+			dup2(fd,1); //El 1 indica que es salida de escritura
 		}
 	}
 	
@@ -48,12 +49,12 @@ void *redireccionDeError(tline *mandato){
 			fprintf(stderr,"%s: Error. Fallo al crear el fichero para su escritura",(*mandato).redirect_error);
 			exit(1);
 		}else{
-			dup2(fd,2); //El 2 indica que es de error
+			dup2(fd,2); //El 2 indica que es salida de error
 		}
 	}
 }
 
-void uncomando(tline *mandatos){
+void unComando(tline *mandatos){ //un solo comando o mandato por instruccion
 	pid_t pid;
 	tcommand *mandato = (*mandatos).commands;
 	pid=fork();
@@ -80,18 +81,20 @@ void uncomando(tline *mandatos){
 		} //fin padre
 }//fin unmandato
 
-void varioscomandos(tline *mandatos){
+void variosComandos(tline *mandatos){ //n mandatos con el uso de pipes
 	tcommand *mandato = (*mandatos).commands;
 	int comandos = (*mandatos).ncommands;
 	tPipe pipes[comandos-1];
-	int pids [comandos];
+	pid_t pids [comandos];
 	int i;
 	tPipe p1;
 	tPipe p2;
-	for (i=0; i<comandos-1; i++){
+	for (i = 0; i < comandos-1; i++){
 		pipe(pipes[i].p);
  	}// fin for creaccion de pipes
-		for(i = 0; i < comandos; i++){//creamos tantos hijos como
+	signal(SIGQUIT, SIG_DFL);
+	signal(SIGINT, SIG_DFL);
+	for(i = 0; i < comandos; i++){//creamos tantos hijos como comandos tenga la instrucción
 		pids[i] = fork();	  
 		if(pids[i] < 0){ //si no se puede crear el hijo
 			fprintf(stderr,"Ha ocurrido un error al crear el proceso");
@@ -99,48 +102,52 @@ void varioscomandos(tline *mandatos){
 		}// fin if de error
 
 		if(pids[i] == 0){ //para el hijo i
-			if(i == 0){
+			if(i == 0){ //primer mandato
 				redireccionDeEntrada(mandatos);
 				p1=pipes[i];
-				dup2(p1.p[1],1);
+				dup2(p1.p[1],1); //se va a escribir en la entrada del pipe-...
 				close(p1.p[0]);
 				close(p1.p[1]);
-				execvp(mandato[i].filename, mandato[i].argv);
+				execvp(mandato[i].filename, mandato[i].argv); //...-la ejecucion del primer mandato
 				exit(1);
 
-			}else if((i != 0) && (i<comandos-1)){
+			}else if((i != 0) && (i<comandos-1)){ //mandato intermedio (ni el primero ni el último)
 				p1=pipes[i-1];
 				p2=pipes[i];
-				dup2(p1.p[0],0);
+				dup2(p1.p[0],0); //se va a leer de la salida del pipe anterior (i-1)
 				close(p1.p[0]);
 				close(p1.p[1]);
-				dup2(p2.p[1],1);
+				dup2(p2.p[1],1); //se va a escribir en la entrada del siguiente pipe (i)-...
 				close(p2.p[0]);
 				close(p2.p[1]);
-				execvp(mandato[i].filename, mandato[i].argv);
+				execvp(mandato[i].filename, mandato[i].argv);//...-la ejecucion del mandato i
 				exit(1);
 			}
-			else{
-			     redireccionDeSalida(mandatos);
+			else{ //último mandato
+			     	redireccionDeSalida(mandatos);
+				redireccionDeError(mandatos);
 				p1=pipes[i-1];
-				dup2(p1.p[0],0);
+				dup2(p1.p[0],0); //se va a leer de la salida del último pipe-...
 				close(p1.p[0]);
 				close(p1.p[1]);
-				execvp(mandato[i].filename, mandato[i].argv);
+				execvp(mandato[i].filename, mandato[i].argv);//...-y se va a ejecutar el último mandato
+				//de esta forma, el último mandato recibirá las salidas de todos los mandatos anteriores para 
+				//asi ejecutarse exactamente como queremos.
 				exit(1);
 			}
-			redireccionDeError(mandatos);
 		}// fin del pid==0
 	}//fin del for 		
 	wait(NULL);
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, SIG_IGN);
 }// fin del varios comandos
 
 void cd(tline *mandatos){
 	tcommand *mandato = (*mandatos).commands;
-	if((*mandato).argv[1] == NULL){
-		chdir(getenv("HOME"));
+	if((*mandato).argv[1] == NULL){ //si no recibe un directorio
+		chdir(getenv("HOME")); //el comando cd nos llevará al directorio home
 	}else{
-		chdir((*mandato).argv[1]);
+		chdir((*mandato).argv[1]); //en caso contrario cambiamos al directorio especificado
 	}
 }
 
@@ -150,25 +157,24 @@ int main(int argc, char* argv[]){ //inicio main
 	signal(SIGINT, SIG_IGN);
 	tline *mandatos; //variable para hacerle el tokenize
 	char buffer[1024];
-	printf("msh> ");
+	printf("msh> "); //prompt
 	if (argc == 1) {
 		while (fgets(buffer, 1024, stdin)) { //inicio de while
-			//recogemos en buffer la variable de entrada de teclado
+			//recogemos en buffer la entrada de teclado
 			printf("msh> ");			
-			mandatos = tokenize(buffer);
-			if ((*mandatos).ncommands == 1) {
-				tcommand *mandato = (*mandatos).commands;
-				if(strcmp((*mandato).argv[0],"cd") == 0){
+			mandatos = tokenize(buffer); //con tokenize, transformamos dicha entrada en un tline
+			if ((*mandatos).ncommands == 1) { //si la instruccion es de un solo comando
+				tcommand *mandato = (*mandatos).commands; //se crea esta variable para la comprobacion de argv[0]
+				if(strcmp((*mandato).argv[0],"cd") == 0){ //comprobamos si dicho comando es el comando cd
 					cd(mandatos);
 				}else{
-					uncomando(mandatos);
+					unComando(mandatos);
 				}
 
-			} else if ((*mandatos).ncommands > 1) {
-				varioscomandos(mandatos);
+			} else if ((*mandatos).ncommands > 1) { //si la instruccion leida contiene más de un mandato (usa pipes)
+				variosComandos(mandatos);
 			}
 		} //fin del while
 	} else //fin del if
 		fprintf(stderr,"Error, funcion sin argumentos.\n");
 } //fin del main
-
